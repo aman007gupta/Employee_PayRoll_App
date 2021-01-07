88c563e8b95bdd717bdd2a9f756d367eb59bc156
aman007gupta <aman9453122969@gmail.com>
2021-01-07 12:22:00 +0530

    [Aman]Added: code for view employee payroll detail from local storage

--- js/home.js
+++ js/home.js
@@ -1,16 +1,26 @@
+let employeePayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    employeePayrollList = getEmployeePayrollFromStorage();
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
 });
 
+const getEmployeePayrollFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? 
+                        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
+
 const createInnerHtml = () => {
+    if (employeePayrollList.length == 0)
+        return;
     const headerHtml = `<th></th><th>Name</th><th>Gender</th><th>Department</th>` +
                         `<th>Salary</th><th>Start Date</th><th>Actions</th>`;
     let innerHtml = `${headerHtml}`;
-    let employeePayrollList = createEmployeePayrollJSON();                    
+    // let employeePayrollList = createEmployeePayrollJSON();                    
     for (const employeePayrollData of employeePayrollList) {
         innerHtml = `${innerHtml}
                 <tr>
-                    <td class="profile" alt="" src="${employeePayrollData._profilePic}">
+                    <td class="profile"  src="${employeePayrollData._profilePic} alt=""">
                     </td>
                     <td>${employeePayrollData._name}</td>
                     <td>${employeePayrollData._gender}</td>
@@ -29,9 +39,9 @@ const createInnerHtml = () => {
 const getDeptHtml = (deptList) => {
     let deptHtml = '';
     for (const dept of deptList) {
-        deptHtml = `${deptHtml}<div class='dept-label'>${dept}<div>`
+        deptHtml = `${deptHtml}<div class='dept-label'>${dept}<div>`;
     }
-    return deptHtml
+    return deptHtml;
 }
 
 const createEmployeePayrollJSON = () => {
